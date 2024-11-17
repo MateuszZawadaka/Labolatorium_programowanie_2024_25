5802adc388d414584bcf96fcb25bb0f9948f9009
Mateusz Zawadka <w69869@student.wsiz.edu.pl>
2024-11-17 09:34:39 +0100

    workin on excercises

---
+++ zad_3.py
@@ -0,0 +1,13 @@
+x = int(input("Ilość osób: "))
+n = int(input("Ilość potraw: "))
+cena = 0
+i = 0
+kwota = 0
+while i <= n:
+    cena = float(input(f"Podaj cene potrawy{i}: "))
+    kwota = kwota + x * cena
+    i += 1
+
+print(f"Kwota za wszytko{kwota}")
+na_osobe = kwota / x
+print(f"Kwota na osobę: {na_osobe}")
